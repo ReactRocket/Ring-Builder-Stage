478f9bde8cb1687395d81eacd4c6afc6ba12d336
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-03-14 16:53:43 +0530

    Add react-router-dom and update package.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-
+/* eslint-disable react/jsx-pascal-case */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Diamond from "./pages/diamond/Diamond";
+import Setting from "./pages/setting/Setting";
+import Complete from "./pages/complete/Complete";
+import Error_404 from "./pages/error/Error_404";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Diamond />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/complete" element={<Complete />} />
+        <Route path="*" element={<Error_404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
