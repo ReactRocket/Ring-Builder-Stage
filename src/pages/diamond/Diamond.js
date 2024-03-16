6d3d02354ee3e64b76270bc7abf9fb219f9f30b4
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-03-16 13:53:23 +0530

    Product Table View Created.

--- src/pages/diamond/Diamond.js
+++ src/pages/diamond/Diamond.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 import ChooseDiamonds from "../../components/ChooseDiamonds";
-
+import ProductTableView from "../../components/ProductTableView";
 
 const Diamond = () => {
   return (
     <div className="min-h-screen max-w-screen  ">
       <div className="h-[90%] w-[85%] flex-col gap-5  py-10 mx-auto flex justify-start items-center">
         <Breadcrumb />
-        <ChooseDiamonds/>
+        <ChooseDiamonds />
 
-        
+        <ProductTableView />
       </div>
     </div>
   );
