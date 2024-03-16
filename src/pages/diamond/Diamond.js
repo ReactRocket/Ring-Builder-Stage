11d8c26ec8ab5e64905eb39cc084f84eb3005b08
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-03-16 13:00:31 +0530

    Added ChooseDiamonds component and updated Diamond page.

--- src/pages/diamond/Diamond.js
+++ src/pages/diamond/Diamond.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Breadcrumb from "../../components/Breadcrumb";
+import ChooseDiamonds from "../../components/ChooseDiamonds";
+
 
 const Diamond = () => {
   return (
     <div className="min-h-screen max-w-screen  ">
       <div className="h-[90%] w-[85%] flex-col gap-5  py-10 mx-auto flex justify-start items-center">
         <Breadcrumb />
+        <ChooseDiamonds/>
+
         
       </div>
     </div>
