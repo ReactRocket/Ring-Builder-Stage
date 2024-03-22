9f4b6a8ff518f9eb3a6fd0853b807e0eca8219e0
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-03-22 18:27:34 +0530

    "Add Complete page component with CompletedProduct and OrderSummary subcomponents; update index.css with Poppins font face"
    This commit includes the following changes:
    
    1. The `Complete` component is added to `src/pages/complete/Complete.js`. It now returns a layout containing a `CompletedProduct` component and an `OrderSummary` component.
    2. The `CompletedProduct` and `OrderSummary` components are added to `src/pages/complete/subcomponents/CompletedProduct.js` and `src/pages/complete/subcomponents/OrderSummary.js`, respectively.
    3. The `index.css` file is updated to include a new `@font-face` rule for the Poppins font.

--- src/pages/complete/Complete.js
+++ src/pages/complete/Complete.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CompletedProduct from "./subcomponents/CompletedProduct";
+import OrderSummary from "./subcomponents/OrderSummary";
 
 const Complete = () => {
   return (
-    <div>Complete</div>
-  )
-}
+    <div className="min-h-screen w-full p-20">
+      <div className="min-h-[5vh] w-full py-">
+        <h1 className=" font-['Lato', sans-serif] font-[500] text-[18px] text-[#804294]">
+          My Cart (2 items)
+        </h1>
+      </div>
+      <div className="h-[95vh] w-full flex gap-5">
+        <div className="h-full w-2/3 ">
+          <CompletedProduct />
+        </div>
+        <div className="h-full w-1/3 ">
+          <OrderSummary />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Complete
+export default Complete;
